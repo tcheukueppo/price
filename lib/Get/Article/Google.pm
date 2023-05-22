7af3a78a7c29541191157d4972da1d1fa4a40f5c
tcheukueppo <tcheukueppo@tutanota.com>
2023-05-22 19:10:03 +0000

    update text modifier tags, recursively get text content of the div and p tags with _get_text()

--- lib/Get/Article/Google.pm
+++ lib/Get/Article/Google.pm
@@ -1,9 +1,15 @@
 package Get::Article::Google;
 
+use strict;
+use warnings;
+use utf8;
+
+no warnings 'utf8';
+
 use Mojo::UserAgent;
 use Mojo::URL;
 
-use Carp qw(croak carp);
+use Carp         qw(croak carp);
 use Scalar::Util qw(refaddr);
 
 use Data::Dumper;
@@ -17,22 +23,24 @@ my $TARGET_LINK = qr{
    )
 }x;
 
-my $TEXT_MODIFIERS = do {
-   '^' . join(
+my $CONTENT_TAGS        = qr/^(?:div|span|p)$/;
+my $TEXT_MODIFIERS_TAGS = do {
+   '^(?:' . join(
       '|', qw(
-         a abbr area b bdi bdo cite data 
-         datalist del dfn em i ins kbd keygen
-         label map mark meter object output
-         progress q ruby s samp slot small span
-         strong sub sup time u var wbr acronym
-         applet basefont big tt
+         h1 h2 h3 h4 h5 h6 big
+         a abbr b bdi bdo cite
+         del dfn em i ins kbd acronym
+         data label mark meter tt
+         output progress q ruby
+         s samp slot small strong
+         sub sup time u var wbr
         )
      )
-     . '$';
+     . ')$';
 };
 
 sub new {
-   carp 'send key-value mojo ua options' if @_ % 2 == 0;
+   carp 'Err: please sent key-value Mojo::UserAgent options' if @_ % 2 == 0;
    my ($class, %args) = @_;
 
    my $self = {ua => Mojo::UserAgent->new()};
@@ -71,7 +79,8 @@ sub google {
      ->head($n);
 
    $self->{index} = 0;
-   $self->{links} = [$c->each];
+   $self->{links} = [$c->shuffle->each];
+   say Dumper $self->{links};
    return 1;
 }
 
@@ -83,33 +92,58 @@ sub prev_link {
    $_[0]->{index} == 0 ? -1 : $_[0]->{index}--;
 }
 
+sub _get_text {
+   my $node = shift;
+
+   no warnings 'recursion';
+   my $text = $node
+     ->child_nodes
+     ->map(
+        sub {
+           $_->type eq 'text'
+           ? $_->content
+           : defined($_->tag) ? (
+                $_->tag =~ /$TEXT_MODIFIERS_TAGS/ ? $_->text
+              : $_->tag =~ $CONTENT_TAGS          ? _get_text($_)
+              :                                     ''
+           )
+           : '';
+        }
+     )
+     ->compact
+     ->join(' ') =~ s/^\s+//r =~ s/\s+$//r =~ s/\s{2,}/ /gr;    # Beautify
+
+   return $text;
+}
+
 sub get_content {
    my $self = shift;
    my $link = $self->{links} && $self->{links}[++$self->{index}];
 
    return unless $link;
 
+   $link = 'https://dir.indiamart.com/impcat/hp-computer-monitor.html';
    my $tx = $self->{ua}->get($link);
    return -1 unless $tx->result->is_success;
 
    my $content;
    my $index = 0;
-   foreach my $p ($tx->result->dom->find('p')) {
+   foreach my $node ($tx->result->dom->find('*')->each) {
+      next if $node->type eq 'text' or $node->tag !~ $CONTENT_TAGS;
+      my $text = _get_text($node);
 
-      $content->[$index]{c}{id}   = refaddr($p);
-      $content->[$index]{c}{text} = $p
-         ->child_nodes
-         ->grep(sub { $_->type eq 'text' or $_->tag =~ $TEXT_MODIFIERS })
-         ->map('content')
-         ->join('') =~ s/^\s+//r =~ s/\s+$//r =~ s/\s{2,}|\R/ /r;
+      next unless $text;
 
-      $content->[$index++]{c_ids} = [$p
-         ->child_nodes
-         ->grep(sub { $_->tag eq 'p' })
-         ->map(sub { refaddr($_) })
-         ->each
-      ];
+      say "#####";
+      say $text, "\n";
+      say "#####";
+      $content->[$index]{text} = $text;
+      $content->[$index]{id}   = refaddr($node);
+      $content->[$index]{type} = $node->tag;
+
+      $index++;
    }
 
+   # ...
    return $content;
 }
