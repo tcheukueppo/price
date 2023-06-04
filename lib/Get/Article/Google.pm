fcc4b885198be84458d7893e6c6c249f2a559f63
tcheukueppo <tcheukueppo@tutanota.com>
2023-06-04 21:20:17 +0000

    New method for extracting contents

--- lib/Get/Article/Google.pm
+++ lib/Get/Article/Google.pm
@@ -3,6 +3,7 @@ package Get::Article::Google;
 use strict;
 use warnings;
 use utf8;
+use feature qw(say state);
 
 no warnings 'utf8';
 
@@ -14,32 +15,36 @@ use Carp         qw(croak carp);
 use Scalar::Util qw(refaddr);
 
 use Data::Dumper;
-use feature qw(say);
 
-my $index       = -1;
-my $NO_GOOGLE   = qr{https://(?!(?>\w+\.)*google\.com)};
-my $TARGET_LINK = qr{
+my $ANY_QUANTITY = qr~(\S+)\s*\d(?>(?:[\d.,] ?)+)?\d?\s*(?(1)|\S+)~;
+my $NO_GOOGLE    = qr~https://(?!(?>\w+\.)*google\.com)~;
+my $TARGET_LINK  = qr~
    (?|
       imgrefurl = ((?> [^&]+ ))
     | (?:q|u)   = ( $NO_GOOGLE (?>[^&]+) )
    )
-}x;
+~x;
 
-my $NESTED_TAGS         = qr/^(?:div|p|span)$/;
+my $NESTED_TAGS = qr/^(?:div|p|span)$/;
+
+# Tags which modify strings characteristics
 my $TEXT_MODIFIERS_TAGS = do {
-   '^(?:' . join(
-      '|', qw(
-         h1 h2 h3 h4 h5 h6 big
-         a abbr b bdi bdo cite
-         del dfn em i ins kbd acronym
-         data label mark meter tt
-         output progress q ruby
-         s samp slot small strong
-         sub sup time u var wbr
-        )
-     )
-     . ')$';
+   local $" = '|';
+
+   my @modifiers = qw(
+      h1 h2 h3 h4 h5 h6 big
+      a abbr b bdi bdo cite
+      del dfn em i ins kbd acronym
+      data label mark meter tt
+      output progress q ruby
+      s samp slot small strong
+      sub sup time u var wbr
+   );
+
+   qr/^(?:@modifiers)$/;
 };
+
+my ($index, $max_length) = (-1, 50);
 
 sub new {
    bless {
@@ -60,7 +65,7 @@ sub google {
    return unless $article;
 
    $n //= 100;
-   $self->{url}->query(q => "prix de $article");
+   $self->{url}->query(q => "price of $article");
 
    my $tx = $self->{ua}->get("$self->{url}");
    return 0 if !$tx->result->is_success;
@@ -82,7 +87,6 @@ sub google {
 
    $index = -1;
    $self->{links} = [$c->each];
-   say Dumper $self->{links};
    return 1;
 }
 
@@ -106,8 +110,8 @@ sub get_contents {
 
    my $content;
    $content->{lang} = $res->dom->at('html')->attr('lang') // 'en';
-   foreach my $node ($res->dom->find('div, p')->each) {
-      my $text = _get_text($node);
+   foreach my $node ($res->dom->at('body')->find('div, p')->each) {
+      my $text = _get_text($node, 1);
 
       next unless "$text";
       push @{$content->{text}}, ref $text ? $text->to_string : $text;
@@ -117,7 +121,42 @@ sub get_contents {
    return $content;
 }
 
+sub _beautify {
+   my $text = "$_[0]" =~ s/^\s+//r =~ s/\s+$//r =~ s/\s{2,}/ /gr;
+   1 == length $text ? '' : $text
+}
+
 sub _get_text {
+   my $parent = $_[0];
+   state($text, $params);
+
+   no warnings 'recursion';
+
+   $text = '', $params = [] if defined $_[1];    # top level
+   foreach my $node ($parent->child_nodes->each) {
+      if ($node->type eq 'text') {
+         next unless length my $node_text = _beautify($_->content);
+
+         if    ($node_text =~ $ANY_QUANTITY)                               { push @$params, ${^MATCH} }
+         elsif ($node_text =~ /\.$/ and length($node_text) => $max_length) { $text .= "\n$node_text" }
+      }
+      elsif (defined($_->tag)) {
+         if    ($_->tag =~ $NESTED_TAGS) { _get_text($_) }
+         elsif ($_->tag =~ $TEXT_MODIFIERS_TAGS) {
+            next unless length my $node_text = _beautify($_->all_text);
+            $text .= "\n" if $_->tag =~ /^h{1,6}$/;
+            $text .= $node_text;
+         }
+      }
+   }
+
+   if (defined($_[1]) && length($text) >= $max_length) {
+      local $" = ' ';
+      return "@$params\n$text";
+   }
+}
+
+sub _lget_text {
    my $node = shift;
 
    my $get_text = sub {
